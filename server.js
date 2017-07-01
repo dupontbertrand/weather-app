d0706e84edb0d7c27fbf247b889afde2b89327be
dupontbertrand <dupontbertrand59@gmail.com>
2017-07-01 02:41:05 +0200

    change server.js and ejs, some wording too

--- server.js
+++ server.js
@@ -1,22 +1,36 @@
-const express = require('express')
-const app = express()
+const express = require('express');
 const bodyParser = require('body-parser');
-
-app.set('view engine', 'ejs');
+const request = require('request');
+const app = express()
+const apiKey = 'e4159415289bc1562bef2e28100d14d2';
 
 app.use(express.static('public'));
-
 app.use(bodyParser.urlencoded({ extended: true }));
+app.set('view engine', 'ejs')
 
 app.get('/', function (req, res) {
-  res.render('index');
+  res.render('index', {weather: null, error: null});
 })
 
 app.post('/', function (req, res) {
-  res.render('index');
-  console.log(req.body.city);
+  let city = req.body.city;
+  let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${apiKey}`
+
+  request(url, function (err, response, body) {
+    if(err){
+      res.render('index', {weather: null, error: 'Oups, une erreur est survenue. Réessayez !'});
+    } else {
+      let weather = JSON.parse(body)
+      if(weather.main == undefined){
+        res.render('index', {weather: null, error: 'Oups, une erreur est survenue. Réessayez !'});
+      } else {
+        let weatherText = `Il fait ${weather.main.temp} degrés à ${weather.name}!`;
+        res.render('index', {weather: weatherText, error: null});
+      }
+    }
+  });
 })
 
 app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+  console.log('Weather app tourne sur le port 3000 ! :)')
 })

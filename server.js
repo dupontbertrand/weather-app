65ee1bc63d60a3e9fc038191d7eceb855b60e654
dupontbertrand <dupontbertrand59@gmail.com>
2017-07-01 02:24:26 +0200

    add public folder and css

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express')
 const app = express()
 
-app.set('view engine', 'ejs')
+app.set('view engine', 'ejs');
+
+app.use(express.static('public'));
 
 app.get('/', function (req, res) {
-  res.send('Hello World!')
+  res.render('index');
 })
 
 app.listen(3000, function () {
